361a00b051a3d96727552483f7cdde51dbbf997c
DongHyeokJeong <ddkkd1@naver.com>
2017-02-07 02:16:18 +0900

    add nickName function

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+let login_users = {};
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
-    socket.on('entrance', function(data) {  // 들어올 때 알림
-        io.emit('entranceAlarm', {id:socket.id});
+    socket.on('entrance', function (data) {  // 들어올 때 알림
+        login_users[socket.id] = data.nickName;
+        io.emit('entranceAlarm', {nickName:login_users[socket.id]});
     });
 
     socket.on('disconnect', function() {  // 나갈 때 알림
-        io.emit('exitAlarm', {id:socket.id});
+        io.emit('exitAlarm', {nickName:login_users[socket.id]});
     });
 
     socket.on('chat message', function(msg){
